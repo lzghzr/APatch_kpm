99f00c3d949a1d1c54fe6442b0ca3997fee283f7
lzghzr <me@lzzr.me>
2024-03-21 00:10:03 +0800

    添加 KPM_CTL0

--- xperia_ii_battery_age.c
+++ xperia_ii_battery_age.c
@@ -3,12 +3,16 @@
  * Copyright (C) 2023 lzghzr. All Rights Reserved.
  */
 
+#include <compiler.h>
+#include <hook.h>
 #include <kpmodule.h>
+#include <kputils.h>
+#include <linux/kernel.h>
 #include <linux/printk.h>
-#include <syscall.h>
+#include <log.h>
 
 KPM_NAME("xperia_ii_battery_age");
-KPM_VERSION("1.0.0");
+KPM_VERSION("1.1.0");
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("lzghzr");
 KPM_DESCRIPTION("xperia ii set battery aging level");
@@ -23,26 +27,56 @@ int (*fg_sram_read)(struct fg_dev *fg, u16 address, u8 offset, u8 *val, int len,
 int (*fg_sram_write)(struct fg_dev *fg, u16 address, u8 offset, u8 *val, int len, int flags) = 0;
 
 u8 aging = 0;
+struct fg_dev *fg = 0;
+
+static long inline_hook_control0(const char *args, char *__user out_msg, int outlen)
+{
+    aging = args ? *args - '0' : 0;
+    if (aging > 5)
+    {
+        return -1;
+    }
+    int rc = fg_sram_write(fg, SOMC_AGING_LEVEL_WORD, SOMC_AGING_LEVEL_OFFSET, &aging, 1, FG_IMA_DEFAULT);
+    char echo[64] = "";
+    if (rc < 0)
+    {
+        sprintf(echo, "error, rc=%d\n", rc);
+        logke("fg_sram_write %s", echo);
+        if (out_msg)
+        {
+            compat_copy_to_user(out_msg, echo, sizeof(echo));
+            return 1;
+        }
+    }
+    else
+    {
+        sprintf(echo, "success, set batt_aging_level to %d\n", aging);
+        logki("fg_sram_write %s", echo);
+        if (out_msg)
+        {
+            compat_copy_to_user(out_msg, echo, sizeof(echo));
+            return 0;
+        }
+    }
+}
 
 void before_read(hook_fargs6_t *args, void *udata)
 {
     unhook(fg_sram_read);
-    struct fg_dev *fg = (struct fg_dev *)args->arg0;
+    fg = (struct fg_dev *)args->arg0;
     // u8 *arg3 = (u8 *)args->arg3;
     // logkd("before read fg: %llu, address: %u, offset: %u, val: %u, len: %d, flags: %d\n", args->arg0, (u16)args->arg1,
     //       (u8)args->arg2, (u8)*arg3, (int)args->arg4, (int)args->arg5);
-    int rc = fg_sram_write(fg, SOMC_AGING_LEVEL_WORD, SOMC_AGING_LEVEL_OFFSET, &aging, 1, FG_IMA_DEFAULT);
-    if (rc < 0)
-        logke("fg_sram_write error, rc=%d\n", rc);
-    else
-        logki("fg_sram_write success, set batt_aging_level to %u\n", aging);
+    inline_hook_control0(&aging, NULL, NULL);
 }
 
-int inline_hook_init(const char *args)
+static long inline_hook_init(const char *args, const char *event, void *__user reserved)
 {
     aging = args ? *args - '0' : 0;
     if (aging > 5)
+    {
         return -1;
+    }
 
     fg_sram_read = (typeof(fg_sram_read))kallsyms_lookup_name("fg_sram_read");
     pr_info("kernel function fg_sram_read addr: %llx\n", fg_sram_read);
@@ -51,7 +85,9 @@ int inline_hook_init(const char *args)
     pr_info("kernel function fg_sram_write addr: %llx\n", fg_sram_write);
 
     if (!fg_sram_read || !fg_sram_write)
+    {
         return -2;
+    }
 
     hook_err_t err = hook_wrap6(fg_sram_read, before_read, 0, 0);
     if (err)
@@ -66,11 +102,14 @@ int inline_hook_init(const char *args)
     }
 }
 
-void inline_hook_exit()
+static long inline_hook_exit(void *__user reserved)
 {
     if (fg_sram_read)
+    {
         unhook(fg_sram_read);
+    }
 }
 
 KPM_INIT(inline_hook_init);
+KPM_CTL0(inline_hook_control0);
 KPM_EXIT(inline_hook_exit);
