58adb86800e50b0f250b4efaaeca2440ff4826ab
lzghzr <me@lzzr.me>
2024-03-30 21:01:18 +0800

    与自动化工具配合

--- hosts_redirect/hosts_redirect.c
+++ hosts_redirect/hosts_redirect.c
@@ -1,36 +1,47 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Copyright (C) 2023 lzghzr. All Rights Reserved.
+ * Copyright (C) 2024 lzghzr. All Rights Reserved.
  */
 
-#include <asm/current.h>
 #include <compiler.h>
 #include <hook.h>
 #include <kpmodule.h>
 #include <kputils.h>
-#include <linux/err.h>
 #include <linux/fs.h>
 #include <linux/printk.h>
 #include <linux/string.h>
-#include <linux/uaccess.h>
-#include <syscall.h>
 #include <taskext.h>
 
 KPM_NAME("hosts_redirect");
-KPM_VERSION("1.0.0");
+KPM_VERSION("1.1.0");
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("lzghzr");
-KPM_DESCRIPTION("redirect /system/etc/hosts to /data/adb/hosts");
+KPM_DESCRIPTION("redirect /system/etc/hosts to /data/adb/hosts/{n}");
 
 struct open_flags;
-int (*do_filp_open)(int dfd, struct filename *pathname, const struct open_flags *op) = 0;
+uint64_t (*do_filp_open)(int dfd, struct filename *pathname, const struct open_flags *op) = 0;
 
 char hosts_source[] = "/system/etc/hosts";
-char hosts_target[] = "/data/adb/hosts";
+char hosts_target[] = "/data/adb/hosts/0";
+
+static long inline_hook_control0(const char *ctl_args, char *__user out_msg, int outlen)
+{
+  char num = ctl_args ? *ctl_args : '1';
+  if (unlikely(num < '0' || num > '9'))
+  {
+    return -11;
+  }
+  hosts_target[16] = num;
+  return 0;
+}
 
 void do_filp_open_before(hook_fargs3_t *args, void *udata)
 {
   args->local.data0 = 0;
+  if (unlikely(hosts_target[16] == '0'))
+  {
+    return;
+  }
   char **fname = *(char ***)args->arg1;
   if (unlikely(!memcmp(fname, hosts_source, sizeof(hosts_source))))
   {
@@ -56,18 +67,24 @@ void do_filp_open_after(hook_fargs3_t *args, void *udata)
 
 static long inline_hook_init(const char *args, const char *event, void *__user reserved)
 {
+  int rc = inline_hook_control0(args, NULL, NULL);
+  if (rc < 0)
+  {
+    return rc;
+  }
+
   do_filp_open = (typeof(do_filp_open))kallsyms_lookup_name("do_filp_open");
   pr_info("kernel function do_filp_open addr: %llx\n", do_filp_open);
   if (!do_filp_open)
   {
-    return -1;
+    return -21;
   }
 
   hook_err_t err = hook_wrap3(do_filp_open, do_filp_open_before, do_filp_open_after, 0);
   if (err)
   {
     pr_err("hook do_filp_open after error: %d\n", err);
-    return -2;
+    return -22;
   }
   else
   {
@@ -82,10 +99,9 @@ static long inline_hook_exit(void *__user reserved)
   if (do_filp_open)
   {
     unhook(do_filp_open);
-    do_filp_open = 0;
   }
 }
 
 KPM_INIT(inline_hook_init);
-// KPM_CTL0(inline_hook_control0);
+KPM_CTL0(inline_hook_control0);
 KPM_EXIT(inline_hook_exit);
