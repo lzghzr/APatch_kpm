31c260917f0858913bd34d1b7059ab7b8ac0ceb4
lzghzr <me@lzzr.me>
2024-06-16 22:17:50 +0800

    [lmkd_dont_kill] 去除不必要的条件

--- lmkd_dont_kill/lmkd_dont_kill.c
+++ lmkd_dont_kill/lmkd_dont_kill.c
@@ -17,7 +17,7 @@
 #include "lmkd_dont_kill.h"
 
 KPM_NAME("lmkd_dont_kill");
-KPM_VERSION(LDK_VERSION);
+KPM_VERSION(MYKPM_VERSION);
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("lzghzr");
 KPM_DESCRIPTION("lmkd_dont_kill");
@@ -30,45 +30,38 @@ static int (*do_send_sig_info)(int sig, struct siginfo* info, struct task_struct
 
 char lmkd[] = "lmkd";
 static void do_send_sig_info_before(hook_fargs4_t* args, void* udata) {
-    int sig = (int)args->arg0;
-    struct kernel_siginfo* siginfo = (struct kernel_siginfo*)args->arg1;
-    struct task_struct* dst = (struct task_struct*)args->arg2;
+  int sig = (int)args->arg0;
+  struct kernel_siginfo* siginfo = (struct kernel_siginfo*)args->arg1;
+  struct task_struct* dst = (struct task_struct*)args->arg2;
 
-    if (sig != SIGKILL || siginfo->si_code != 0)
-        return;
-    if (task_uid(current).val < MIN_SYSTEM_UID
-        || task_uid(dst).val < MIN_USERAPP_UID
-        || task_uid(current).val == task_uid(dst).val)
-        return;
+  if (sig != SIGKILL || siginfo->si_code != 0)
+    return;
 
-    const char* comm = get_task_comm(current);
-    if (!memcmp(comm, lmkd, sizeof(lmkd) - 1)) {
-        args->ret = -EPERM;
-        args->skip_origin = true;
-    }
+  const char* comm = get_task_comm(current);
+  if (!memcmp(comm, lmkd, sizeof(lmkd) - 1)) {
+    args->ret = -EPERM;
+    args->skip_origin = true;
+  }
 }
 
-static long inline_hook_init(const char* args, const char* event, void* __user reserved)
-{
-    lookup_name(do_send_sig_info);
-    hook_func(do_send_sig_info, 4, do_send_sig_info_before, 0, 0);
+static long inline_hook_init(const char* args, const char* event, void* __user reserved) {
+  lookup_name(do_send_sig_info);
+  hook_func(do_send_sig_info, 4, do_send_sig_info_before, 0, 0);
 
-    return 0;
+  return 0;
 }
 
-static long inline_hook_control0(const char* ctl_args, char* __user out_msg, int outlen)
-{
-    char msg[64];
-    snprintf(msg, sizeof(msg), "_(._.)_");
-    compat_copy_to_user(out_msg, msg, sizeof(msg));
-    return 0;
+static long inline_hook_control0(const char* ctl_args, char* __user out_msg, int outlen) {
+  char msg[64];
+  snprintf(msg, sizeof(msg), "_(._.)_");
+  compat_copy_to_user(out_msg, msg, sizeof(msg));
+  return 0;
 }
 
-static long inline_hook_exit(void* __user reserved)
-{
-    unhook_func(do_send_sig_info);
+static long inline_hook_exit(void* __user reserved) {
+  unhook_func(do_send_sig_info);
 
-    return 0;
+  return 0;
 }
 
 KPM_INIT(inline_hook_init);
